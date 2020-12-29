eddc02ac49c2feddda382e8f9d39bef1a28be4a3
Noah Schwartz <noah.amir.schwartz@gmail.com>
2020-12-29 18:37:59 -0500

    Added Resize and Reset Button

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
-let size = 20;
+let size = 16;
 let container = document.getElementsByClassName("container")[0];
 updateGridSize(size);
 function updateGridSize(size){
+    container.innerHTML = "";
     container.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
     container.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
     for(let i = 0; i < size**2; i++){
         container.innerHTML = container.innerHTML + "<div></div>";
     }
-    for(let i = 0; i < container.childNodes.length; i++){
-        container.childNodes[i].addEventListener("mouseenter", function(){
-            container.childNodes[i].style.backgroundColor = 'red';
-        })
+    container.childNodes.forEach(addEvent);
+    container.childNodes.forEach(resetColor);
+        
 }
+function getSize(){
+    updateGridSize(prompt("Please enter a size(less than 100)"));
 }
+
+function addEvent(div){
+    div.addEventListener("mouseenter", function(){
+        div.style.backgroundColor = 'red';
+    });
+}
+function resetColorOnClick(){
+    container.childNodes.forEach(resetColor)
+     
+}
+function resetColor(div){
+    div.style.backgroundColor = 'black';
+} 
 
