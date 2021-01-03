0df803f69fc52c67a481d8d0e120daa0afef1903
Noah Schwartz <noah.amir.schwartz@gmail.com>
2021-01-02 23:15:26 -0500

    Added rainbow colors option

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 let size = 16;
 let color = 'red';
-let defaultColor = 'gray';
+let defaultColor = 'white';
+let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 let container = document.getElementsByClassName("container")[0];
+let checkbox = document.getElementById("rainbow");
+checkbox.addEventListener('change', function(){
+    let colorpicker = document.getElementById("colorpicker");
+    if(this.checked === true){
+        colorpicker.style.opacity = '50%';
+        colorpicker.disabled = true;
+    }
+    else{
+        document.getElementById("colorpicker").style.opacity = '100%';
+        colorpicker.disabled = false;
+    }
+    updateGridSize(size);
+    })
+
 updateGridSize(size);
 function updateGridSize(size){
     document.getElementById("size-button").innerText = "Size: " + size + "x" + size;
@@ -15,17 +30,25 @@ function updateGridSize(size){
     container.childNodes.forEach(resetColor);
         
 }
+
 function getSize(){
-    updateGridSize(prompt("Please enter a size(less than 100)"));
+    size = prompt("Please enter a size(less than 100)")
+    updateGridSize(size);
 }
 
 function addEvent(div){
-    div.addEventListener("mouseenter", function(){
-        div.style.backgroundColor = color;
-    });
+    
+    if(checkbox.checked === true){
+        div.addEventListener("mouseenter", setRainbow);
+    }
+    else{
+        div.addEventListener("mouseenter", setColor);
+}
+
 }
 function resetColorOnClick(){
-    container.childNodes.forEach(resetColor)
+    updateGridSize(size);
+    
      
 }
 function resetColor(div){
@@ -36,5 +59,25 @@ function clickColor(e){
     color = e.value;
     container.childNodes.forEach(addEvent);
     container.childNodes.forEach(resetColor);
+}
+
+function setRainbow(){
+    
+    this.style.backgroundColor = rainbowColors[Math.floor(Math.random()*7)];
+    setTimeout(removeEvent(this), 0);
+}
+
+function setColor(){
+    this.style.backgroundColor = color;
+    setTimeout(removeEvent(this), 0);
+}
+
+function removeEvent(div){
+     if(checkbox.checked === true){
+        div.removeEventListener("mouseenter", setRainbow);
+     }
+     else{
+        div.removeEventListener("mouseenter", setColor);
+     }
 }
 
